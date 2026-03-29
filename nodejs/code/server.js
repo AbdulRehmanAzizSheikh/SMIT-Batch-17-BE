e9cf50e371b968fb02436253ee7c94ef29120149
abdulrehmanazizf5 <abdulrehmanaziz5f@gmail.com>
2026-03-29 14:42:08 +0500

    feat: initialize basic HTTP server with routing for shirt and shoe endpoints

--- nodejs/code/server.js
+++ nodejs/code/server.js
@@ -2,35 +2,35 @@
 
 // console.log(username)
 
-const fs = require("fs");
-// const http = require("http");
-// const url = require("url");
+// const fs = require("fs")
+const http = require("http");
+const url = require("url");
 
-const readFileReturn = fs.readFile("./server.txt", "utf-8", (err, data) => {
-  console.log(data);
-});
+// const readFileReturn = fs.readFile("./server.txt", "utf-8", (err, data) => {
+//     console.log(data)
+// })
 
-fs.writeFileSync("./server.txt", "kuch bi bhaee dimagh mai ni arhaa!");
+// fs.writeFileSync("./server.txt", "kuch bi bhaee dimagh mai ni arhaa!")
 
-console.log(readFileReturn);
+// console.log(readFileReturn)
 
 /// Server create ker rahy hain...
 
-// const server = http.createServer((req, res) => {
-//     const pathname = req.url
-//     // console.log(req.url)
-//     if(pathname == "/shirt"){
-//         res.end("shirt wala page")
-//     } else if(pathname == "/shoe"){
-//         res.end("shoe wala page")
-//     }else {
-//      console.log("something went wrong!")
-//     }
-// res.end("server is running -->")
-// })
+const server = http.createServer((req, res) => {
+  const pathname = req.url;
+  // console.log(req.url)
+  if (pathname == "/shirt") {
+    res.end("shirt wala page");
+  } else if (pathname == "/shoe") {
+    res.end("shoe wala page");
+  } else {
+    console.log("something went wrong!");
+  }
+  res.end("server is running -->");
+});
 
-// // console.log(server)
+// console.log(server)
 
-// server.listen(8000, "127.0.0.1", ()=>{
-//     console.log("server is listening!")
-// })
+server.listen(8000, "127.0.0.1", () => {
+  console.log("server is listening!");
+});
